7e0578795c4a4297f54cab1542cc1855d51186b7
John Weissenberger <johnaweissenberger@gmail.com>
2018-12-06 12:49:24 -0500

    finish intersect

---
+++ Prolog/intersection.pl
@@ -0,0 +1,12 @@
+mymember(X, [X|XS]).
+mymember(X, [Y|YS]):-
+    mymember(X,YS).
+
+intersect([X|Y], A, [X|B]):-
+    mymember(X,A),
+    intersect(Y,A,B).
+intersect([X|Y],A,B):-
+    \+ mymember(X,A),
+    intersect(Y,A,B).
+intersect([],A,[]).
+
